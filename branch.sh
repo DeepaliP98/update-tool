4669d08af13e4bb1d794ca1d979c51f6d4b48123
Parth Krishna <thenerdsuperuser@gmail.com>
2020-06-08 16:42:27 +0530

    Added menu

--- branch.sh
+++ branch.sh
@@ -9,8 +9,20 @@ then
 	echo "Upstream URL: $UPSTREAM ";
 else
 	echo "Upstream is not added.";
+	echo "Let us set upstream first."
+	upstream
 fi
 
+upstream() {
+	echo -e "Enter the URL of the original repo: \c"
+	read REPO
+
+	echo "Let's add upstream to your local repo as a remote."
+	git remote add upstream $REPO
+	sleep 2
+	echo "Remote upstream set."
+
+}
 
 update() {
 	echo "Updating ..."
@@ -18,14 +30,8 @@ update() {
 	git push $ORIGIN master
 
 }
-other() {
-	echo "This program will help you add your changes to git."
-	echo -e "Enter the URL of the original repo: \c"
-	read REPO
 
-	echo "Perfect. Now, let's add it to your local repo as a remote."
-	git remote add upstream $REPO
-
+feature() {
 	echo "Done. Now, let's get a branch made for you."
 	echo -e "Enter the name of the branch you want to create: \c"
 	read BRANCH
@@ -33,3 +39,25 @@ other() {
 	git checkout $BRANCH
 
 }
+
+
+PS3='Please enter your choice: '
+options=("Update" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Update")
+					update
+					;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
